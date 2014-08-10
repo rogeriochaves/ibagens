84625b5d460ec81a6740b33df193c6cca3cb7843
Rogerio Chaves <rogeriocfj@gmail.com>
2014-08-10 11:40:53 -0300

    stop if there is too much elelements

--- ibagens/bookmarklet.js
+++ ibagens/bookmarklet.js
@@ -2,10 +2,12 @@
 	var fixWords = function(){
 		var all = document.getElementsByTagName("*");
 
-		for (var i = 0, max = all.length; i < max; i++) {
-		     var el = all[i];
-		     el.innerHTML = el.innerHTML.replace(/(<.+?>[^<>]*?)(i)(mage)([^<>]*?<.+?>)/i, "$1$2bage$4");
-		     // It's correct now (:
+		if(all.length < 2000){ // too much elements!
+			for (var i = 0, max = all.length; i < max; i++) {
+			     var el = all[i];
+			     el.innerHTML = el.innerHTML.replace(/(<.+?>[^<>]*?)(i)(mage)([^<>]*?<.+?>)/i, "$1$2bage$4");
+			     // It's correct now (:
+			}
 		}
 	}
 
