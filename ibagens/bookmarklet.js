b8610d3a24160dd691391720ee7843883f0cf2cc
Rogerio Chaves <rogeriocfj@gmail.com>
2014-09-05 20:37:44 -0300

    refactored, doesn't break  events now

--- ibagens/bookmarklet.js
+++ ibagens/bookmarklet.js
@@ -1,20 +1,31 @@
 (function(){
-	var fixWords = function(){
-		var all = document.getElementsByTagName("*");
 
-		if(all.length < 2000){ // too much elements!
-			for (var i = 0, max = all.length; i < max; i++) {
-			     var el = all[i];
-			     if(el.innerHTML.match(/(<.+?>[^<>]*?)(i)(mage)([^<>]*?<.+?>)/i)){
-			     	el.innerHTML = el.innerHTML.replace(/(<.+?>[^<>]*?)(i)(mage)([^<>]*?<.+?>)/i, "$1$2bage$4");
-			     }
-			     // It's correct now (:
+	var fixElement = function(elem){
+		var childs = elem.childNodes;
+		if(childs.length < 2000){
+			for(var i = 0, max = childs.length; i < max; i++){
+				var child = childs[i];
+				if(child.nodeType == 3 && child.nodeValue.match(/(i)(mage)/i)){
+					child.nodeValue = child.nodeValue.replace(/(i)(mage)/i, "$1bage");
+				}
+				if(child.childNodes.length > 0 && child.tagName !== "SCRIPT" && child.tagName !== "STYLE"){
+					fixElement(child);
+				}
 			}
 		}
+	}
+
+	var fixWords = function(){
+		var head = document.getElementsByTagName("head")[0];
+		var body = document.getElementsByTagName("body")[0];
+
+		fixElement(head);
+		fixElement(body);
 	}
 
 	document.onreadystatechange = function(){
 	    if (document.readyState === 'complete') fixWords();
 	}
 	window.onload = fixWords;
+	
 })();
